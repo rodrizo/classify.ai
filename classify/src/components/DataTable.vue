e06ed1082d58d2b62051794ce9e4c6164eb1a73a
Rodney Rizo <rodneyrizo@outlook.com>
2024-05-28 01:57:46 -0400

    last changes

---
+++ classify/src/components/DataTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-responsive
+      class="align-centerfill-height mx-auto mt-10"
+      max-width="900"
+    >
+      <v-data-table 
+        :headers="headers"
+        :items="values"
+      >
+      </v-data-table>
+    </v-responsive>
+
+  </v-container>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      this.getData()
+    },
+    props: {
+      results: Array // Prop para recibir el array de usuarios
+    },
+    data: () => ({
+      headers: [
+        { align: 'start', key: 'id', title: 'Id'},
+        { key: 'text', title: 'Texto' },
+        { key: 'classification', title: 'Clasificacion' },
+        { key: 'emoji', title: 'Emoción' },
+      ],
+      values: [
+        {
+          id: '',
+          text: '',
+          classification: '',
+          emoji: '',
+        }
+      ],
+    }),
+    methods: {
+      getData(){
+        console.log("showing datatable")
+        console.log(this.results)
+        this.values = this.results;
+      }
+    }
+  };
+</script>
---
+++ classify/src/components/Test.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-form
+      @submit.prevent="addEmotion"
+      ref="form"
+  >
+  <v-btn
+          color="success"
+          class="me-4 mt-4"
+          type="submit"
+          prepend-icon="mdi-plus"
+          rounded="lg"
+        >
+          Crear
+        </v-btn>
+  
+  </v-form>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      overlay: false,
+      result: ''
+    }),
+
+    watch: {
+      overlay (val) {
+        val && setTimeout(() => {
+          this.overlay = false
+        }, 2000)
+      },
+    },
+    methods: {
+      
+      async addEmotion() {
+        await fetch('https://0712bf0e-f0df-4299-850c-16e09f87b347.mock.pstmn.io/',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          // body: JSON.stringify(this.text)
+        }).then(response => response.json()).then(x => {
+          this.result = x
+          console.log(x)
+        });
+
+
+      }
+    }
+  }
+</script>
